3d720d39a3cd07379128a04987f367e0aa50bf45
pipka2021 <78415261+pipka2021@users.noreply.github.com>
2021-09-10 20:16:07 +0300

    done

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,221 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.html;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections;
+using System.Diagnostics;
+
+namespace PizzaEd
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoadExel_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog choofdlog = new OpenFileDialog();
+            choofdlog.Filter = "ЭКСЕЛЬ ФАЙЛЫ! (*.xlsx)|*.xlsx";
+            if (choofdlog.ShowDialog() == true)
+            {
+                string sFileName = choofdlog.FileName;
+                string path = System.IO.Path.GetFullPath(choofdlog.FileName);
+                Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+                DataSet ds = new DataSet();
+                Microsoft.Office.Interop.Excel.Workbook wb = excel.Workbooks.Open(path);
+                foreach (Microsoft.Office.Interop.Excel.Worksheet ws in wb.Worksheets)
+                {
+                    System.Data.DataTable td = new System.Data.DataTable();
+                    td = await Task.Run(() => formofDataTable(ws));
+                    ds.Tables.Add(td);//This will give the DataTable from Excel file in Dataset
+                }
+                DataGridMeow.ItemsSource = ds.Tables[0].DefaultView;
+                sortButton.IsEnabled = true;
+                saveButton.IsEnabled = true;
+                //LoadExel.IsEnabled = false;
+                wb.Close();
+            }
+        }
+
+        public System.Data.DataTable formofDataTable(Microsoft.Office.Interop.Excel.Worksheet ws)
+        {
+            System.Data.DataTable dt = new System.Data.DataTable();
+            string worksheetName = ws.Name;
+            dt.TableName = worksheetName;
+            Microsoft.Office.Interop.Excel.Range xlRange = ws.UsedRange;
+            object[,] valueArray = (object[,])xlRange.get_Value(Microsoft.Office.Interop.Excel.XlRangeValueDataType.xlRangeValueDefault);
+            for (int k = 1; k <= valueArray.GetLength(1); k++)
+            {
+                dt.Columns.Add((string)valueArray[1, k]);  //add columns to the data table.
+            }
+            object[] singleDValue = new object[valueArray.GetLength(1)]; //value array first row contains column names. so loop starts from 2 instead of 1
+            for (int i = 2; i <= valueArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < valueArray.GetLength(1); j++)
+                {
+                    if (valueArray[i, j + 1] != null)
+                    {
+                        singleDValue[j] = valueArray[i, j + 1].ToString();
+                    }
+                    else
+                    {
+                        singleDValue[j] = valueArray[i, j + 1];
+                    }
+                }
+                dt.LoadDataRow(singleDValue, System.Data.LoadOption.PreserveChanges);
+            }
+
+            return dt;
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            sortButton.IsEnabled = false;
+            saveButton.IsEnabled = false;
+
+        }
+        public static void SortDataGrid(DataGrid dataGrid, int columnIndex = 0, ListSortDirection sortDirection = ListSortDirection.Ascending)
+        {
+            var column = dataGrid.Columns[columnIndex];
+
+            // Clear current sort descriptions
+            dataGrid.Items.SortDescriptions.Clear();
+
+            // Add the new sort description
+            dataGrid.Items.SortDescriptions.Add(new SortDescription(column.SortMemberPath, sortDirection));
+
+            // Apply sort
+            foreach (var col in dataGrid.Columns)
+            {
+                col.SortDirection = null;
+            }
+            column.SortDirection = sortDirection;
+
+            // Refresh items to display sort
+            dataGrid.Items.Refresh();
+        }
+        private void sortButton_Click(object sender, RoutedEventArgs e)
+        {
+                SortDataGrid(DataGridMeow);
+        }
+
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog choofdlog = new SaveFileDialog();
+            choofdlog.Filter = "ПЭДЭЭФ ФАЙЛЫ! (*.pdf)|*.pdf";
+            if (choofdlog.ShowDialog() == true)
+            {
+
+                ExportToPdf(DataGridMeow, choofdlog.FileName);
+                MessageBox.Show("Файл успешно сохранён.", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+                Process PrFolder = new Process();
+                ProcessStartInfo psi = new ProcessStartInfo();
+                string file = choofdlog.FileName;
+                psi.CreateNoWindow = true;
+                psi.WindowStyle = ProcessWindowStyle.Normal;
+                psi.FileName = "explorer";
+                psi.Arguments = @"/n, /select, " + file;
+                PrFolder.StartInfo = psi;
+                PrFolder.Start();
+            }
+            
+        }
+        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj)
+       where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+                    if (child != null && child is T)
+                    {
+                        yield return (T)child;
+                    }
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                    {
+                        yield return childOfChild;
+                    }
+                }
+            }
+        }
+
+        public static childItem FindVisualChild<childItem>(DependencyObject obj)
+            where childItem : DependencyObject
+        {
+            foreach (childItem child in FindVisualChildren<childItem>(obj))
+            {
+                return child;
+            }
+
+            return null;
+        }
+        private void ExportToPdf(DataGrid grid, string name)
+        {
+            PdfPTable table = new PdfPTable(grid.Columns.Count);
+            Document doc = new Document(iTextSharp.text.PageSize.LETTER, 10, 10, 42, 35);
+            PdfWriter writer = PdfWriter.GetInstance(doc, new System.IO.FileStream(name, System.IO.FileMode.Create));
+            doc.Open();
+
+            string ARIALUNI_TFF = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), "arial.TTF");
+
+            //Create a base font object making sure to specify IDENTITY-H
+            BaseFont bf = BaseFont.CreateFont(ARIALUNI_TFF, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+
+            //Create a specific font object
+            Font f = new Font(bf, 12, Font.NORMAL);
+            for (int j = 0; j < grid.Columns.Count; j++)
+            {
+                table.AddCell(new Phrase(grid.Columns[j].Header.ToString(), f));
+            }
+            table.HeaderRows = 1;
+            IEnumerable itemsSource = grid.ItemsSource as IEnumerable;
+            if (itemsSource != null)
+            {
+                foreach (var item in itemsSource)
+                {
+                    DataGridRow row = grid.ItemContainerGenerator.ContainerFromItem(item) as DataGridRow;
+                    if (row != null)
+                    {
+                        DataGridCellsPresenter presenter = FindVisualChild<DataGridCellsPresenter>(row);
+                        for (int i = 0; i < grid.Columns.Count; ++i)
+                        {
+                            DataGridCell cell = (DataGridCell)presenter.ItemContainerGenerator.ContainerFromIndex(i);
+                            TextBlock txt = cell.Content as TextBlock;
+                            if (txt != null)
+                            {
+                                table.AddCell(new Phrase(txt.Text,f));
+                            }
+                        }
+                    }
+                }
+
+                doc.Add(table);
+                doc.Close();
+            }
+        }
+    }
+}
